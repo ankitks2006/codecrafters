ed3222a7ed684e3941c791aa75b5b2aadea639cc
Ankit <ankit@gmail.com>
2026-07-04 15:37:08 +0530

    smpt2

--- backend/src/services/emailService.js
+++ backend/src/services/emailService.js
@@ -22,6 +22,10 @@ const transporter = nodemailer.createTransport({
   requireTLS: true,
 });
 // add here
+console.log("EMAIL_HOST =", process.env.EMAIL_HOST);
+console.log("EMAIL_PORT =", process.env.EMAIL_PORT);
+console.log("EMAIL_USER =", process.env.EMAIL_USER);
+console.log("EMAIL_PASS =", process.env.EMAIL_PASS);
 transporter.verify((error, success) => {
   if (error) {
     console.error("SMTP Verify Error:", error);
